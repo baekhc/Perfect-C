2a20911b1d2e5b47f6cbffb9993232a515a4ab6f
baekhc <qor3296@naver.com>
2016-09-27 17:23:03 +0900

    2, 3, 4 소스추가
    각단원 추가

---
+++ Ch02/L1basictype.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 30, b = 43;
+	int sum;
+	sum = a + b;
+
+	double x = 38.342, y = 45.345;
+	double DIF;
+	DIF = x - y;
+
+	printf("Че %d\n", sum);
+	printf("Тї %f\n", DIF);
+
+	return 0;
+}
